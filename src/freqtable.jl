adf408dbac29e7e5aabf8d21db8d19e95e457238
Milan Bouchet-Valat <nalimilan@club.fr>
2014-06-25 22:14:07 +0200

    Better handling of the 'subset' argument
    This makes the code cleaner, and in the future will rely on
    array views. Dispatching on the presence of subset would be
    better yet (positional argument?).

--- src/freqtable.jl
+++ src/freqtable.jl
@@ -1,8 +1,16 @@
 import Base.ht_keyindex
 
 function freqtable(x::AbstractVector...;
-                   subset::Union(Nothing, AbstractVector{Int}, AbstractVector{Bool}, BitVector) = nothing,
-                   weights::Union(Nothing, AbstractVector{Number}) = nothing)
+                   weights::Union(Nothing, AbstractVector{Number}) = nothing,
+                   subset::Union(Nothing, AbstractVector{Int}, AbstractVector{Bool}) = nothing)
+    if subset != nothing
+        x = [sub(y, subset) for y in x]
+
+        if weights != nothing
+            weights = sub(weights, subset)
+        end
+    end
+
     n = length(x)
     l = map(length, x)
     vtypes = map(eltype, x)
@@ -11,10 +19,6 @@ function freqtable(x::AbstractVector...;
         if l[1] != l[i]
             error("arguments are not of the same length: $l")
         end
-    end
-
-    if (isa(subset, AbstractVector{Bool}) || isa(subset, BitVector)) && length(subset) != l[1]
-        error("'subset' (length $(length(subset))) must be of the same length as vectors (length $(l[1])) when it is boolean")
     end
 
     if weights != nothing && length(weights) != l[1]
@@ -27,50 +31,22 @@ function freqtable(x::AbstractVector...;
         d = Dict{vtypes, eltype(weights)}()
     end
 
-    if isa(subset, AbstractVector{Bool}) || isa(subset, BitVector)
-        for i in 1:l[1]
-            subset[i] || continue
+    for el in zip(x...)
+        index = ht_keyindex(d, el)
 
-            el = ntuple(n, j -> x[j][i])
-            index = ht_keyindex(d, el)
-
-            if weights == nothing
-                if index > 0
-                    d.vals[index] += 1
-                else
-                    d[el] = 1
-                end
+        if weights == nothing
+            if index > 0
+                d.vals[index] += 1
             else
-                if index > 0
-                    d.vals[index] += weights[i]
-                else
-                    d[el] = weights[i]
-                end
+                d[el] = 1
             end
-        end
-    else
-        if subset == nothing
-            subset = 1:l[1]
-        end
+        else
+            @inbounds w = weights[i]
 
-        for i in subset
-            @inbounds el = ntuple(n, j -> x[j][i])
-            index = ht_keyindex(d, el)
-
-            if weights == nothing
-                if index > 0
-                    d.vals[index] += 1
-                else
-                    d[el] = 1
-                end
+            if index > 0
+                d.vals[index] += w
             else
-                @inbounds w = weights[i]
-
-                if index > 0
-                    d.vals[index] += w
-                else
-                    d[el] = w
-                end
+                d[el] = w
             end
         end
     end
@@ -97,8 +73,16 @@ function freqtable(x::AbstractVector...;
 end
 
 function freqtable2(x::AbstractVector...;
-                    subset::Union(Nothing, AbstractVector{Int}, AbstractVector{Bool}, BitVector) = nothing,
-                    weights::Union(Nothing, AbstractVector{Number}) = nothing)
+                    weights::Union(Nothing, AbstractVector{Number}) = nothing,
+                    subset::Union(Nothing, AbstractVector{Int}, AbstractVector{Bool}) = nothing)
+    if subset != nothing
+        x = [sub(y, subset) for y in x]
+
+        if weights != nothing
+            weights = sub(weights, subset)
+        end
+    end
+
     n = length(x)
     l = map(length, x)
     vtypes = map(eltype, x)
@@ -107,10 +91,6 @@ function freqtable2(x::AbstractVector...;
         if l[1] != l[i]
             error("arguments are not of the same length: $l")
         end
-    end
-
-    if (isa(subset, AbstractVector{Bool}) || isa(subset, BitVector)) && length(subset) != l[1]
-        error("'subset' (length $(length(subset))) must be of the same length as vectors (length $(l[1])) when it is boolean")
     end
 
     if weights != nothing && length(weights) != l[1]
@@ -128,47 +108,23 @@ function freqtable2(x::AbstractVector...;
     a = Array(Int, ntuple(n, i -> 0))
     el = cell(n)
 
-    if isa(subset, AbstractVector{Bool}) || isa(subset, BitVector)
-        for i in 1:l[1]
-            subset[i] || continue
+    for i in 1:l[1]
+        for j in 1:n
+            @inbounds dj = d[j]
+            @inbounds xji = x[j][i]
 
-            for j in 1:n
-                @inbounds pos = findfirst(d[j], x[j][i])
+            pos = findfirst(dj, xji)
 
-                if pos == 0
-                    @inbounds push!(d[j], x[j][i])
-                    a = cat(j, a, zeros(Int, ntuple(n, k -> k == j ? 1 : size(a, k))))
-                    @inbounds pos = length(d[j])
-                end
-
-                @inbounds el[j] = pos
+            if pos == 0
+                push!(dj, xji)
+                a = cat(j, a, zeros(Int, ntuple(n, k -> k == j ? 1 : size(a, k))))
+                pos = length(dj)
             end
 
-            @inbounds a[el...] += 1
-        end
-    else
-        if subset == nothing
-            subset = 1:l[1]
+            @inbounds el[j] = pos
         end
 
-        for i in subset
-            for j in 1:n
-                @inbounds dj = d[j]
-                @inbounds xji = x[j][i]
-
-                pos = findfirst(dj, xji)
-
-                if pos == 0
-                    push!(dj, xji)
-                    a = cat(j, a, zeros(Int, ntuple(n, k -> k == j ? 1 : size(a, k))))
-                    pos = length(dj)
-                end
-
-                @inbounds el[j] = pos
-            end
-
-            @inbounds a[el...] += 1
-        end
+        @inbounds a[el...] += 1
     end
 
     NamedArray(a, ntuple(n, i -> d[i]), ntuple(n, i -> "Dim$i"))
